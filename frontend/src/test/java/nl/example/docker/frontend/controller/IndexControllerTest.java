34d8b6b3d82f11ec585c97af53d08eed9f55701d
Mark Wigmans <mark.wigmans@capgemini.com>
2020-10-06 11:07:22 +0200

    Frontend integration test added

---
+++ frontend/src/test/java/nl/example/docker/frontend/controller/IndexControllerTest.java
@@ -0,0 +1,55 @@
+package nl.example.docker.frontend.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @Container
+    private static final BrowserWebDriverContainer Chrome = new BrowserWebDriverContainer().withCapabilities(new ChromeOptions());
+
+    private static WebDriver browser;
+
+    @LocalServerPort
+    private int port;
+
+    private String baseUrl;
+
+    @BeforeAll
+    static void configureBrowser() {
+        browser = Chrome.getWebDriver();
+    }
+
+    @BeforeEach
+    void init() {
+        baseUrl = "http://host.docker.internal:" + port;
+    }
+
+    @Test
+    @DisplayName("Loading Angular App")
+    void testLaunchAngular() {
+        browser.get(baseUrl + "/");
+        assertThat(browser.getTitle(), is("Docker Demo - Frontend"));
+    }
+
+    @Test
+    @DisplayName("Forward Angular URL's")
+    void testForwardAngular() {
+        browser.get(baseUrl + "/unknown-to-spring");
+        assertThat(browser.getTitle(), is("Docker Demo - Frontend"));
+    }
+}

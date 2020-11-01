fab4b40f60549a453d14fe1cc5a8bc9be5d6024f
Mark Wigmans <mark.wigmans@capgemini.com>
2020-11-01 13:12:23 +0100

    correct image references

--- backend/src/main/java/nl/example/docker/backend/BackendApplication.java
+++ backend/src/main/java/nl/example/docker/backend/BackendApplication.java
@@ -3,12 +3,8 @@ package nl.example.docker.backend;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Docker Demo - Backend", version = "v1", description = "REST interface voor Docker Demo Backend"))
@@ -16,12 +12,8 @@ import java.util.TimeZone;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
         // Oracle driver doesn't like timezone 'etc/UTC', this prevents this.
         System.setProperty("oracle.jdbc.timezoneAsRegion", "false");
+        SpringApplication.run(BackendApplication.class, args);
     }
 }
